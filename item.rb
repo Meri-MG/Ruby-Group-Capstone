fd39c2243bc4a6c6202e9dff92d035bf7ebae7c8
Meri <mgogi13@freeuni.edu.ge>
2022-03-15 18:45:38 +0400

    Apply required changes

--- item.rb
+++ item.rb
@@ -1,17 +1,13 @@
 class Item
-  attr_accessor :id, :publish_date, :archived
-  attr_reader :label, :genre, :author, :source
+  @@id = 1
+  attr_accessor :publish_date
+  attr_reader :id, :archived, :label, :genre, :author, :source
 
   def initialize(id, publish_date, archived: false)
-    @id = id || Random.rand(1..100)
+    @id = @@id
+    @@id += 1
     @publish_date = Date.parse(publish_date)
     @archived = archived
-  end
-
-  def can_be_archived?
-    current_date = Date.today
-    year = current_date.year - @publish_date.year
-    year >= 10
   end
 
   def move_to_archive
@@ -20,27 +16,29 @@ class Item
 
   def label=(label)
     @label = label
-    label.items.push(self) unless label.items.include?(self)
+    @label.add_item(self) unless @label.items.include?(self)
   end
 
   def genre=(genre)
     @genre = genre
-    genre.add_item(self) unless genre.items.include?(self)
+    @genre.add_item(self) unless @genre.items.include?(self)
   end
 
   def author=(author)
     @author = author
-    author.items.push(self) unless author.items.include?(self)
+    @author.add_item(self) unless @author.items.include?(self)
   end
 
   def source=(source)
     @source = source
-    source.items.push(self) unless source.items.include?(self)
+    @source.add_item(self) unless @source.items.include?(self)
+  end
+
+  private
+
+  def can_be_archived?
+    current_date = Date.today
+    year = current_date.year - @publish_date.year
+    year >= 10
   end
 end
-
-new_item = Item.new(12, '2020/10/19')
-new_item.can_be_archived?
-
-puts new_item.can_be_archived?
-puts new_item.move_to_archive
