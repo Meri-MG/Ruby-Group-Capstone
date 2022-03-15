d7affad65ebc7c2eae96272b73a85e1a3796fb96
Meri <mgogi13@freeuni.edu.ge>
2022-03-15 18:46:55 +0400

    Fix linter error

--- item.rb
+++ item.rb
@@ -1,9 +1,11 @@
+require 'date'
+
 class Item
   @@id = 1
   attr_accessor :publish_date
   attr_reader :id, :archived, :label, :genre, :author, :source
 
-  def initialize(id, publish_date, archived: false)
+  def initialize(publish_date, archived: false)
     @id = @@id
     @@id += 1
     @publish_date = Date.parse(publish_date)
