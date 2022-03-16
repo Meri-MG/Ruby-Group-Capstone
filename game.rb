75cb0d694246ad3257b66f7eede5261c839d0ebc
lily-coder <lilyondie@gmail.com>
2022-03-17 01:26:10 +0300

    Fix linter errors

--- game.rb
+++ game.rb
@@ -8,6 +8,7 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
+
   def to_json(*args)
     {
       JSON.create_id => self.class.name,
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,15 +6,15 @@ describe Game do
       @new_game = Game.new('2018-03-05', 'yes', '2022-03-06')
     end
     it 'Check intsance of game()new_game' do
-        expect(@new_game).to be_instance_of Game
-      end
-  
-      it 'Check if new game is inherited from item' do
-        expect(@new_game).to be_kind_of Item
-      end
-  
-      it 'Make new_game archived false since its from beyond two years ago' do
-        expect(@new_game.move_to_archive).to be false
-      end
+      expect(@new_game).to be_instance_of Game
     end
+
+    it 'Check if new game is inherited from item' do
+      expect(@new_game).to be_kind_of Item
+    end
+
+    it 'Make new_game archived false since its from beyond two years ago' do
+      expect(@new_game.move_to_archive).to be false
+    end
+  end
 end
