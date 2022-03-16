f0df8e206543b5c76e1ac4d9edc17aefffd58f2c
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-03-16 14:03:32 +0100

    Create class for saving data to json and restoring it from json

---
+++ Movies-Source/preserve_data.rb
@@ -0,0 +1,43 @@
+require 'json'
+require 'pry'
+require_relative 'movie'
+require_relative 'source'
+
+module DataLayer
+  def object_to_hash(obj)
+    obj.instance_variables.each_with_object({}) do |var, hash|
+      key = var.to_s.delete('@')
+      value = obj.instance_variable_get(var)
+      value = object_to_hash(value) if %w[Movie Source].include?(value.class.name)
+      hash[key] = value
+      case obj.class.name
+      when 'Movie'
+        hash['type'] = 'Movie'
+      when 'Source'
+        hash['type'] = 'Source'
+      end
+      hash
+    end
+  end
+
+  def hash_to_object(hash, classname)
+    case classname
+    when 'Movie'
+      Movie.new(hash['title'], hash['silent'], hash['publish_date'], hash['archived'])
+    when 'Source'
+      Source.new(hash['name'])
+    end
+  end
+
+  def save_data(filename, data)
+    File.open(filename, 'w') do |file|
+      file.puts data.to_json
+    end
+  end
+
+  def read_data(filename)
+    data = []
+    data = JSON.parse(File.read(filename)) if File.exist?(filename)
+    data
+  end
+end
--- Movies-Source/source.rb
+++ Movies-Source/source.rb
@@ -12,4 +12,9 @@ class Source
     @name = name
     @items = []
   end
+
+  def add_item(item)
+    @items << item
+    item.source = self
+  end
 end
