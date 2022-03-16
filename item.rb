b77d8bd8cf018568d41c748c9bc8bf4b3c9e3a54
Meri Gogichashvili <mgogi13@freeuni.edu.ge>
2022-03-16 14:23:51 +0400

    Project setup, main.rb. Item.rb
    * Update readme file
    
    * Add main.rb file
    
    * Add Item class properties and it's methods
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Apply required changes
    
    * Fix linter error
    
    * Fix linter errors
    
    * Apply all the required changes

---
+++ item.rb
@@ -0,0 +1,53 @@
+require 'date'
+
+# rubocop:disable Style/ClassVars
+
+class Item
+  @@id = 1
+
+  attr_accessor :publish_date
+  attr_reader :id, :archived, :label, :genre, :author, :source
+
+  def initialize(publish_date, archived: false)
+    @id = @@id
+    @@id += 1
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end
+
+  def label=(label)
+    @label = label
+    @label.add_item(self) unless @label.items.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.add_item(self) unless @genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    @author.add_item(self) unless @author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    @source.add_item(self) unless @source.items.include?(self)
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    current_date = Date.today
+
+    return unless current_date.year - @publish_date.year >= 10
+
+    current_date.month < @publish_date.month
+  end
+end
+
+# rubocop:enable Style/ClassVars
---
+++ main.rb
@@ -0,0 +1,73 @@
+# rubocop: disable Metrics
+
+class App
+  def run
+    puts 'Welcome to your Catalog!'
+
+    loop do
+      list_of_options
+
+      option_entry = gets.chomp.downcase
+
+      break if option_entry == '0'
+
+      option_chosen(option_entry)
+    end
+  end
+
+  def list_of_options
+    puts '',
+         'Please choose an option by entering a number:',
+         '1 - List all books',
+         '2 - List all music albums',
+         '3 - List all movies',
+         '4 - List of games',
+         '5 - List all genres (e.g "Comedy", "Thriller")',
+         '6 - List all labels (e.g. "Gift", "New")',
+         '7 - List all authors (e.g. "Stephen King")',
+         '8 - List all sources (e.g. "From a friend", "Online shop")',
+         '9 - Add a book',
+         '10 - Add a music album',
+         '11 - Add a movie',
+         '12 - Add a game',
+         '0 - Exit'
+  end
+
+  def option_chosen(option)
+    case option
+    when '1'
+      puts '1'
+    when '2'
+      puts '2'
+    when '3'
+      puts '3'
+    when '4'
+      puts '4'
+    when '5'
+      puts '5'
+    when '6'
+      puts '6'
+    when '7'
+      puts '7'
+    when '8'
+      puts '8'
+    when '9'
+      puts '9'
+    when '10'
+      puts '10'
+    when '11'
+      puts '11'
+    when '12'
+      puts '12'
+    when '0'
+      puts 'E'
+    else
+      puts 'Seems like an invalid entry!'
+    end
+  end
+end
+
+app = App.new
+app.run
+
+# rubocop: enable Metrics
