873ba85e73d84bb889c2de008dab50fd698296c3
lily-coder <lilyondie@gmail.com>
2022-03-17 01:01:27 +0300

    Set up game class properties

--- game.rb
+++ game.rb
@@ -2,4 +2,10 @@ require './item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
 end
