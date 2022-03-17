77536c51fdb7ae9edc8acad2483ffc046475c4f4
mwanawabangona <ellisallday@gmail.com>
2022-03-17 03:41:03 -0700

    Add album

---
+++ modules/genres_module.rb
@@ -0,0 +1,26 @@
+require 'json'
+require './classes/genre'
+
+module MusicAlbumModule
+  def load_genres
+    data = []
+    file = './json_files/genres.json'
+    if File.exist?(file)
+      JSON.parse(File.read(file)).each do |genre|
+        data.push(Genre.new(genre['id'], genre['name']))
+      end
+    else
+      File.write(file, [])
+    end
+
+    data
+  end
+
+  def create_genre
+    data = []
+    @music_albums.each do |genre|
+      data.push({ id: genre.id, name: genre.name })
+    end
+    open('./json_files/genres.json', 'w') { |f| f << JSON.generate(data) }
+  end
+end
