8356562c114643c98a19c1def792707b391bfda5
Meri Gogichashvili <mgogi13@freeuni.edu.ge>
2022-03-18 15:41:05 +0400

    Merged music_album classes and functionalities
    * Update readme file
    
    * Add main.rb file
    
    * Add Item class properties and it's methods
    
    * Create Book and Label classes with constructors and methods
    
    * Add add_item method to the Label class
    
    * Fix linter errors
    
    * Fix linter errors
    
    * Preserve Data
    
    * Update class functions
    
    * Apply required changes
    
    * Fix linter error
    
    * Fix linter errors
    
    * Apply all the required changes
    
    * Add properties to Book class
    
    * Fix linter errors
    
    * Fix parsing bug for archived property
    
    * Fix linter errors
    
    * Add class cases for Book class
    
    * Add tests cases for label class
    
    * Create schema.sql file for book, item, label classes
    
    * Apply required changes, add list to labels.json file
    
    * Update readme with uml image
    
    * Add item
    
    * Add item
    
    * Add genres
    
    * Add album
    
    * Add json
    
    * Add album
    
    * Update album
    
    * Add Schema
    
    * Fixed bugs
    
    * Apply required changes on shcema.sql
    
    * Update README.md
    
    * Changed folder structure add class var to genre
    
    * Changed Arr to Var
    
    * Update Item & main.rb
    
    * Fix bugs
    
    * Fix Linter bug
    
    * Fix linter errors
    
    Co-authored-by: Bohdan Shcherbak <b.szczerbak@gmail.com>
    Co-authored-by: mwanawabangona <ellisallday@gmail.com>

--- main.rb
+++ main.rb
@@ -1,12 +1,21 @@
 require_relative './Movies-Source/class_functions'
+require_relative './MusicAlbums-Genres/classes/music_album'
+require_relative './MusicAlbums-Genres/modules/music_album_mule'
+require_relative './MusicAlbums-Genres/modules/genres_module'
+require_relative './MusicAlbums-Genres/classes/genre'
+require 'json'
+require './item'
 
 # rubocop: disable Metrics
 
 class App
   include DataLayer
+  include MusicAlbumModule
 
   def initialize
     @functions = Functions.new
+    @music_albums = load_music_albums
+    @genres = load_genres
   end
 
   def run
@@ -37,6 +46,8 @@ class App
       option_chosen(option_entry)
     end
 
+    puts 'Thanks for using our app!'
+
     @functions.save_on_exit
   end
 
@@ -63,13 +74,13 @@ class App
     when '1'
       @functions.list_books
     when '2'
-      puts '2'
+      list_all_music_album
     when '3'
       @functions.list_movies
     when '4'
       puts '4'
     when '5'
-      puts '5'
+      list_all_genres
     when '6'
       @functions.list_labels
     when '7'
@@ -79,7 +90,7 @@ class App
     when '9'
       @functions.create_book
     when '10'
-      puts '10'
+      add_music_album
     when '11'
       @functions.create_movie
     when '12'
@@ -89,6 +100,43 @@ class App
     else
       puts 'Seems like an invalid entry!'
     end
+  end
+
+  def list_all_music_album
+    puts 'Music Albums'
+    puts 'There are no Music albums yet' if @music_albums.empty?
+    @music_albums.each do |music_album|
+      puts "Name: #{music_album.name}, Publish Date: #{music_album.publish_date}, On Spotify: #{music_album.on_spotify}"
+    end
+  end
+
+  def list_all_genres
+    puts 'Genres'
+    load_genres.each do |genre|
+      puts "Genre name: #{genre.name}"
+    end
+  end
+
+  def add_music_album
+    print 'Album name: '
+    name = gets.chomp
+
+    print 'Genre: '
+    album_genre = gets.chomp
+
+    print 'Date of publish [Enter date in format (yyyy-mm-dd)] '
+    publish_date = gets.chomp
+
+    print 'Is it available on Spotify? Y/N '
+    on_spotify = gets.chomp.downcase == 'y' || false
+
+    @music_albums.push(MusicAlbum.new(name, publish_date, on_spotify))
+    puts 'Music album created'
+    create_music_album
+
+    @genres << Genre.new(album_genre)
+    puts 'Genre created successfully'
+    create_genre
   end
 end
 
